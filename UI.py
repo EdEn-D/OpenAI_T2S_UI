96cabeb380f5bc368014ab32fc1ab0e45d4f6103
EdEn-D <72027854+EdEn-D@users.noreply.github.com>
2023-12-10 15:13:33 +0200

    finished 1st itteration of UI
    changed logic to OOP, added checkboxes, select/deselect all buttons

--- UI.py
+++ UI.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 from tkinter import filedialog, ttk, messagebox, Canvas, Scrollbar, Frame
+from tkinter import *
 from os import getcwd
-from OpenAI_T2S_UI.t2s_logic import get_t2s_from_file
+from t2s_logic import T2SConverter
 
-def get_sentences():
-    # Placeholder function that returns a list of sentences
-    return ["Sentence 1", "Sentence 2", "Sentence 3", "Sentence 1", "Sentence 2", "Sentence 3", "Sentence 1", "SenSentenceSentSentenceSentenceSentenceSentenceSentenceSentenceenceSentenceSentenceSentenceSentenceSentenceSentencetence 2", "Sentence 3","Sentence 1", "Sentence 2", "Sentence 3", "Sentence 1", "Sentence 2", "Sentence 3", ]
+# def get_sentences():
+#     # Placeholder function that returns a list of sentences
+#     return ["Sentence 1", "Sentence 2", "Sentence 3", "Sentence 1", "Sentence 2", "Sentence 3", "Sentence 1", "SenSentenceSentSentenceSentenceSentenceSentenceSentenceSentenceenceSentenceSentenceSentenceSentenceSentenceSentencetence 2", "Sentence 3","Sentence 1", "Sentence 2", "Sentence 3", "Sentence 1", "Sentence 2", "Sentence 3", ]
 
 
 def select_file():
@@ -15,18 +16,63 @@ def select_file():
         ('Text files', '*.txt')
     ]
     file_path = filedialog.askopenfilename(filetypes=file_types)
-    file_label.config(text=file_path)
+    if file_path:
+        file_label.config(text=file_path)
+        # Check if the selected file is an Excel file
+        if file_path.endswith(('.xlsx', '.xls')):
+            load_rows_into_ui(file_path)
 
-def directory_created_notification(message):
+checkboxes = []
+
+# Called after file is selected, populates rows of the scrollable area
+def load_rows_into_ui(file_path):
+    global checkboxes
+    checkboxes = []  # Reset the list for new file
+    rows = T2SConverter.load_excel_rows(file_path)
+    # Clear existing checkboxes
+    for widget in scrollable_frame.winfo_children():
+        widget.destroy()
+
+    # Populate checkboxes with new rows
+    for i, sentence in enumerate(rows):
+        row_label = tk.Label(scrollable_frame, text=str(i + 1), bg="white")
+        row_label.grid(row=i, column=0, sticky='w', padx=5)
+
+        chk_var = IntVar()
+        chk = tk.Checkbutton(scrollable_frame, text=sentence, bg="white", variable=chk_var)
+        chk.grid(row=i, column=1, sticky='w', padx=5)
+        chk.select()
+        checkboxes.append(chk_var)  # Store the row number and checkbox variable
+        #print(chk_var.get())
+
+
+    # Resize the window after it's displayed
+    current_width = root.winfo_width()
+    current_height = root.winfo_height()
+    new_width = current_width + 10
+    new_height = current_height
+    root.geometry(f"{new_width}x{new_height}")
+    #root.geometry("810x450")
+
+
+def callback_notification(message):
     # Update the UI here, e.g., show a message
     messagebox.showinfo("Notification", message)
 
 def call_t2s_function():
+    active_lines = []
+    for i, c in enumerate(checkboxes):
+        if c.get() == 1:
+            active_lines.append(i +1)
+
     file_path = file_label.cget("text")
     output_dir = dir_label.cget("text")
     selected_option = combo_box.get()
     if file_path not in ["", "No file selected"]:
-        get_t2s_from_file(file_path, output_dir, selected_option, directory_created_notification)
+        converter = T2SConverter(file_path, output_dir, callback_notification)
+        converter.process_rows(active_lines, selected_option)
+
+    #     get_t2s_from_file(file_path, output_dir, selected_option, callback_notification)
 
 def select_output_directory():
     directory = filedialog.askdirectory(initialdir=getcwd())
@@ -36,7 +82,7 @@ def select_output_directory():
 # Create the main window
 root = tk.Tk()
 root.title("File Selector with T2S")
-root.geometry("600x400")
+root.geometry("800x450")
 
 # Grid configuration for layout
 root.columnconfigure(0, weight=1)  # Buttons column
@@ -68,12 +114,23 @@ combo_box = ttk.Combobox(root, values=combo_values, state="readonly")
 combo_box.current(0)  # Set the first item as the default selection
 combo_box.grid(row=2, column=1, padx=10, pady=10, sticky='w')
 
-##########################################scrollable area##########################################################
+##########################################scrollable area - start##########################################################
 def on_mousewheel(event):
     canvas.yview_scroll(int(-1*(event.delta/120)), "units")
 
+def select_all_checkboxes():
+    for widget in scrollable_frame.winfo_children():
+        if isinstance(widget, tk.Checkbutton):
+            widget.select()
+
+def deselect_all_checkboxes():
+    for widget in scrollable_frame.winfo_children():
+        if isinstance(widget, tk.Checkbutton):
+            widget.deselect()
+
+
 # Scrollable area for checkboxes
-canvas = Canvas(root)
+canvas = Canvas(root,width=500)
 scrollbar = Scrollbar(root, orient="vertical", command=canvas.yview)
 scrollable_frame = Frame(canvas)
 
@@ -83,6 +140,7 @@ canvas.bind_all("<MouseWheel>", on_mousewheel)
 # Configure canvas and scrollbar
 canvas.configure(yscrollcommand=scrollbar.set)
 canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
 
 # Add frame to canvas
 canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
@@ -94,18 +152,13 @@ scrollbar.grid(row=3, column=1, sticky='ns')
 # Set the background color for the scrollable frame
 scrollable_frame.config(bg="white")
 
-# Populate the scrollable frame with checkboxes, text, and row numbers
-sentences = get_sentences()
-for i, sentence in enumerate(sentences):
-    row_label = tk.Label(scrollable_frame, text=str(i + 1), bg="white")
-    row_label.grid(row=i, column=0, sticky='w', padx=5)
+select_all_button = tk.Button(root, text="Select All", command=select_all_checkboxes)
+select_all_button.grid(row=4, column=0, padx=10, pady=10, sticky='ew')
 
-    chk = tk.Checkbutton(scrollable_frame, text=sentence, bg="white")
-    chk.grid(row=i, column=1, sticky='w', padx=5)
-########################################scrollable area############################################################
+deselect_all_button = tk.Button(root, text="Deselect All", command=deselect_all_checkboxes)
+deselect_all_button.grid(row=4, column=1, padx=10, pady=10, sticky='ew')
 
-
-
+########################################scrollable area - end############################################################
 
 # Add a button to perform T2S
 t2s_button = tk.Button(root, text="Get T2S", command=call_t2s_function)
--- t2s_logic.py
+++ t2s_logic.py
@@ -9,44 +9,104 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-client = OpenAI()
+client = OpenAI(api_key="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")
 
-def get_t2s_from_file(file_path, output_dir, selected_option, callback):
-    # Extract the base filename without extension
-    base_name = os.path.splitext(os.path.basename(file_path))[0]
+import openpyxl
+import os
 
-    # Create a new directory name combining base name and selected option
-    new_dir_name = f"{base_name}_{selected_option}"
-    new_dir_path = os.path.join(output_dir, new_dir_name)
+class T2SConverter:
+    def __init__(self, file_path, output_dir, callback_func):
+        self.file_path = file_path
+        self.output_dir = output_dir
+        self.callback_func = callback_func
 
-    os.makedirs(new_dir_path, exist_ok=True)
-    callback(f"Directory '{new_dir_path}' has been created.")
+    @staticmethod
+    def load_excel_rows(file_path):
+        workbook = openpyxl.load_workbook(file_path)
+        sheet = workbook.active
+        rows = []
 
-    # Check the file extension to determine the action
-    if file_path.endswith(('.xlsx', '.xls')):
-        process_excel_file(file_path, new_dir_path, selected_option, callback)
-    elif file_path.endswith('.csv'):
-        process_csv_file(file_path, new_dir_path, selected_option, callback)
-    elif file_path.endswith('.txt'):
-        print_txt_file_contents(file_path, selected_option)
-
-def process_excel_file(file_path, output_dir, selected_option, callback):
-    workbook = openpyxl.load_workbook(file_path)
-    sheet = workbook.active
-
-    for row_num, row in enumerate(sheet.iter_rows(values_only=True), start=1):
-        description, text_for_speech = row[0], row[1]
-        if description and text_for_speech:  # Check if both cells are not empty
-            get_t2s(output_dir, row_num, description, text_for_speech, selected_option)
-    callback("Finished...")
-
-def process_csv_file(file_path, output_dir, selected_option, callback):
-    with open(file_path, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row_num, row in enumerate(reader, start=1):
+        for row_num, row in enumerate(sheet.iter_rows(values_only=True), start=1):
             description, text_for_speech = row[0], row[1]
             if description and text_for_speech:  # Check if both cells are not empty
-                get_t2s(output_dir, row_num, description, text_for_speech, selected_option)
+                rows.append(description)
+
+        return rows
+
+
+    # Create new folder based on filename and voice
+    def process_rows(self, selected_rows, selected_option):
+        # Extract the base filename without extension
+        base_name = os.path.splitext(os.path.basename(self.file_path))[0]
+
+        # Create a new directory name combining base name and selected option
+        new_dir_name = f"{base_name}_{selected_option}"
+        new_dir_path = os.path.join(self.output_dir, new_dir_name)
+
+        if not os.path.exists(new_dir_path):
+            os.makedirs(new_dir_path, exist_ok=True)
+            self.callback_func(f"Directory '{new_dir_path}' has been created.")
+
+        workbook = openpyxl.load_workbook(self.file_path)
+        sheet = workbook.active
+
+        for row_num, row in enumerate(sheet.iter_rows(values_only=True), start=1):
+            if row_num in selected_rows:
+                description, text_for_speech = row[0], row[1]
+                if description and text_for_speech:  # Check if both cells are not empty
+                    self.convert_to_audio(new_dir_path, description, text_for_speech, selected_option, row_num)
+        self.callback_func("Finished...")
+
+    def convert_to_audio(self, path, desc, t2s, voice, row):
+        # print(out_dir, row, desc, t2s, voice)
+        response = client.audio.speech.create(
+            model="tts-1",
+            voice=voice,
+            input=t2s,
+            speed=0.85,
+        )
+        speech_file_path = os.path.join(path, f"{row} {desc}.mp3")
+        response.stream_to_file(speech_file_path)
+        add_silence_to_mp3(speech_file_path)
+        # convert_mp3_to_wav(speech_file_path)
+
+
+# def get_t2s_from_file(file_path, output_dir, selected_option, callback):
+#     # Extract the base filename without extension
+#     base_name = os.path.splitext(os.path.basename(file_path))[0]
+#
+#     # Create a new directory name combining base name and selected option
+#     new_dir_name = f"{base_name}_{selected_option}"
+#     new_dir_path = os.path.join(output_dir, new_dir_name)
+#
+#     os.makedirs(new_dir_path, exist_ok=True)
+#     callback(f"Directory '{new_dir_path}' has been created.")
+#
+#     # Check the file extension to determine the action
+#     if file_path.endswith(('.xlsx', '.xls')):
+#         process_excel_file(file_path, new_dir_path, selected_option, callback)
+#     elif file_path.endswith('.csv'):
+#         process_csv_file(file_path, new_dir_path, selected_option, callback)
+#     elif file_path.endswith('.txt'):
+#         print_txt_file_contents(file_path, selected_option)
+
+# def process_excel_file(file_path, output_dir, selected_option, callback):
+#     workbook = openpyxl.load_workbook(file_path)
+#     sheet = workbook.active
+#
+#     for row_num, row in enumerate(sheet.iter_rows(values_only=True), start=1):
+#         description, text_for_speech = row[0], row[1]
+#         if description and text_for_speech:  # Check if both cells are not empty
+#             get_t2s(output_dir, row_num, description, text_for_speech, selected_option)
+#     callback("Finished...")
+
+# def process_csv_file(file_path, output_dir, selected_option, callback):
+#     with open(file_path, newline='') as csvfile:
+#         reader = csv.reader(csvfile)
+#         for row_num, row in enumerate(reader, start=1):
+#             description, text_for_speech = row[0], row[1]
+#             if description and text_for_speech:  # Check if both cells are not empty
+#                 get_t2s(output_dir, row_num, description, text_for_speech, selected_option)
 
 def print_txt_file_contents(file_path):
     # Read and print the contents of the TXT file
@@ -54,17 +114,17 @@ def print_txt_file_contents(file_path):
         contents = file.read()
         print(contents)
 
-def get_t2s(out_dir, row, desc, t2s, voice):
-    print(out_dir, row, desc, t2s, voice)
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice=voice,
-        input=t2s,
-        speed=0.85,
-    )
-    speech_file_path = os.path.join(out_dir, f"{row} {desc}.mp3")
-    response.stream_to_file(speech_file_path)
-    convert_mp3_to_wav(speech_file_path)
+# def get_t2s(out_dir, row, desc, t2s, voice):
+#     print(out_dir, row, desc, t2s, voice)
+#     response = client.audio.speech.create(
+#         model="tts-1",
+#         voice=voice,
+#         input=t2s,
+#         speed=0.85,
+#     )
+#     speech_file_path = os.path.join(out_dir, f"{row} {desc}.mp3")
+#     response.stream_to_file(speech_file_path)
+#     convert_mp3_to_wav(speech_file_path)
 
 
 def convert_mp3_to_wav(input_file):
@@ -85,3 +145,18 @@ def convert_mp3_to_wav(input_file):
     os.remove(input_file)
 
     return output_file
+
+from pydub import AudioSegment
+
+def add_silence_to_mp3(file_path):
+    # Load the mp3 file
+    audio = AudioSegment.from_mp3(file_path)
+
+    # Create 0.5 seconds of silence
+    silence = AudioSegment.silent(duration=500)  # Duration is in milliseconds
+
+    # Add silence to the beginning of the audio
+    audio_with_silence = silence + audio
+
+    # Overwrite the original file with the new audio
+    audio_with_silence.export(file_path, format="mp3")
